e8725eb78a8cf739da8b4ccbcbc1f6622005c258
Abdoo14785 <abdalla01145854052@gmail.com>
2025-01-16 04:11:35 +0200

    Edit background radio tab

--- lib/tabs/radio/radio_items.dart
+++ lib/tabs/radio/radio_items.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:islamiapp/app_theme.dart';
 import 'package:islamiapp/tabs/radio/radioClass.dart';
 
 class RadioItems extends StatefulWidget {
   RadioItems({required this.radioname});
   final Radioclass radioname;
 
+  double bottom = 0;
   @override
   _RadioItemsState createState() => _RadioItemsState();
 }
@@ -22,77 +24,79 @@ class _RadioItemsState extends State<RadioItems> {
     if (isPlaying) {
       imagePath = 'assets/images/Pause.png';
       backgroundradioitem = 'backgroundradioitemon';
+      widget.bottom = -30;
+    } else {
+      widget.bottom = 0;
     }
     if (speaker) {
       speakerPath = 'assets/images/speakeroff.png';
     }
 
-    return Column(
-      children: [
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: Color(0xffE2BE7F),
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: AssetImage('assets/images/${backgroundradioitem}.png'),
-              fit: BoxFit.fill,
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.15,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: AppTheme.primary,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: widget.bottom,
+            left: -10,
+            right: -10,
+            child: Image.asset(
+              'assets/images/${backgroundradioitem}.png',
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: 133,
-              child: Column(
-                children: [
-                  Text(
-                    widget.radioname.name,
-                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isPlaying = !isPlaying;
-                            });
-                          },
-                          icon: Image.asset(
-                            '${imagePath}',
-                            width: 44,
-                            height: 44,
-                          ),
+          Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                Text(
+                  widget.radioname.name,
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPlaying = !isPlaying;
+                          });
+                        },
+                        icon: Image.asset(
+                          '${imagePath}',
+                          width: 44,
+                          height: 44,
                         ),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              speaker = !speaker;
-                            });
-                          },
-                          icon: Image.asset(
-                            '${speakerPath}',
-                            width: 30,
-                            height: 30,
-                          ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            speaker = !speaker;
+                          });
+                        },
+                        icon: Image.asset(
+                          '${speakerPath}',
+                          width: 30,
+                          height: 30,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ),
-        SizedBox(
-          height: 8,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
